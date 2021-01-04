a78ca2f2b2cb217ba48263d70ca281b2623a8400
Nemanja92 <nemanja92@icloud.com>
2021-01-03 16:43:01 -1000

    Parse Domains

---
+++ lib/ui/courses/courses_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:rwcourses/constants.dart';
+import 'package:rwcourses/model/course.dart';
+import 'package:rwcourses/ui/courses/courses_controller.dart';
+
+class CoursesPage extends StatefulWidget {
+  @override
+  _CoursesPageState createState() => _CoursesPageState();
+}
+
+class _CoursesPageState extends State<CoursesPage> {
+  final _controller = CoursesController();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Course>>(
+        future: _controller.fetchCourses(Constants.allFilter),
+        builder: (context, snapshot) {
+          var courses = snapshot.data;
+          if (courses == null) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          return Text(courses.toString());
+        });
+  }
+}
